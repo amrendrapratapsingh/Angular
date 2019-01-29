768a127f0db4cf999694672ef60d59ff827c2241
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2019-01-30 02:10:34 +0530

    Update democomponent.ts

--- democomponent.ts
+++ democomponent.ts
@@ -1,3 +1,5 @@
+###################### demo.component.ts #################################
+
 import {Component} from '@angular/core';
 
 @Component({
@@ -12,3 +14,68 @@ export class DemoComponent{
     isdisabled = false;
 
 }
+
+######################### app.module.ts ###################
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//import { ProductComponent } from './product.component';
+import {Product1Component} from './product/product1.component';
+import { DemoComponent } from './product/democomponent';
+import { EmployeeComponent } from './employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //ProductComponent,
+    Product1Component,
+    DemoComponent,
+    EmployeeComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [DemoComponent]
+})
+export class AppModule { }
+
+################## Go to index.html and define the Component #####################
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>ShoppingApp</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <app-demo></app-demo>
+</body>
+</html>
+
+
+########################### Run the Application##############
+>ng serve --open
+
+############## Press F12 in Browser and define Component ###################
+>txtName.getAttribute('value');
+"John"
+>txtName.value;
+"John"
+
+########## Change the textBox value "David" ####################
+>txtName.getAttribute('value');
+"John"
+>txt.value();
+"David"
+
+
+
