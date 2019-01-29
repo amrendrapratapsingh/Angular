455a1d9cdd0742d964320a1f456e454fe001fb8a
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2019-01-30 02:11:13 +0530

    Difference between Property and HTML attribute

--- democomponent.ts
+++ democomponent.ts
@@ -1,4 +1,4 @@
-###################### demo.component.ts #################################
+###################### demo.component.ts ##################################
 
 import {Component} from '@angular/core';
 
