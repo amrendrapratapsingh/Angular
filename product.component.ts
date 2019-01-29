2e640ec6469bdaf4558a873220446616be40d382
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2019-01-30 01:47:53 +0530

    Update product.component.ts

--- product.component.ts
+++ product.component.ts
@@ -11,3 +11,56 @@ import {Component} from '@angular/core';
 export class ProductComponent{
      msg="Product Details";
 }
+
+
+                                            #####app.module.ts#######
+                                            
+                                            import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product.component';
+import {Product1Component} from './product/product1.component';
+import { DemoComponent } from './product/democomponent';
+import { EmployeeComponent } from './employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    Product1Component,
+    DemoComponent,
+    EmployeeComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule { }
+
+
+##################### GO to Index.html and add your Component################
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>ShoppingApp</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <my-product></my-product>
+</body>
+</html>
+
+#################Run the Application#####################################
+>ng serve --open
+
